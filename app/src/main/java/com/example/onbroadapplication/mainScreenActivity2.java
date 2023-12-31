fd6a37d2a2d6c1408c6d038dda76117f92d79790
nusratalam123 <nusratalam975@gmail.com>
2024-01-01 03:19:09 +0600

    onbroadingFinish

---
+++ app/src/main/java/com/example/onbroadapplication/mainScreenActivity2.java
@@ -0,0 +1,14 @@
+package com.example.onbroadapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainScreenActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen2);
+    }
+}
